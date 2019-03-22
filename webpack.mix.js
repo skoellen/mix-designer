ea33358a56e233fa223d295c746c1f8b93b50bdc
Stephan Köllen <stephan.koellen@korono.de>
2019-03-22 09:22:07 +0100

    fix initial resource path in order to compile starter files

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 let mix = require('laravel-mix');
 
-mix.js('src/js/app.js', 'public/js/')
-    .sass('src/sass/app.scss', 'public/css/')
+mix.js('resources/js/app.js', 'public/js/')
+    .sass('resources/sass/app.scss', 'public/css/')
     .setPublicPath('public');
 
 // API use cases
